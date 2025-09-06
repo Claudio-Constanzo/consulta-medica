a42fdd4d18b9cf8c9853b35a6a3b39e2eef7d040
Claudio-Constanzo <claudio.constanzo02@gmail.com>
2025-09-06 19:33:30 -0400

    creacion de formulario
    aun faltan las validaciones y crud

--- consulta/urls.py
+++ consulta/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from formulario.views import registro_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('registro/', registro_view, name='registro'), ##ruta de formulario de registro
+
 ]
+
+if settings.DEBUG:
+    from django.conf import settings
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) ##sirve para cargar archivos estaticos en modo desarrollo
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) ##sirve para cargar archivos multimedia en modo desarrollo 
--- formulario/views.py
+++ formulario/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def registro_view(request):
+    return render(request, 'formulario/registro.html')
