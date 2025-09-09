f2cc3c2035028148c0b2eac74f0b520715709b13
Claudio-Constanzo <claudio.constanzo02@gmail.com>
2025-09-09 18:56:30 -0300

    creacion de pagina admin
    para hacer los cruds a doctor, secretaria y pacientes (corregir pequeño error, ya que se encuentra en la carpeta de fichamedica, en vez de la carpeta consulta del proyecto original)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # consulta
 Proyecto consulta medica programacion Back End
+Paso 1: Descargar aplicacion desde GitHub (ingresar link en el futuro)
+Paso 2: Para inicializar el programa abrir cmd, luego ingresar: py manage.py runserver
+//NOTA DEL DEV:  Para agregar una aplicacion: py manage.py startapp "nombreApp" y luego agregarla a settings.py en INSTALLED_APPS
+Antes de iniciar sesion en el admin, asegurarse de hacer las migraciones: python manage.py makemigrations ----> python manage.py migrate
+Para iniciar sesion en el admin: crear superusuario (python manage.py createsuperuser) ingresar nombre de usuario, mail(opcional) y contraseña segun los requisitos de django, luego iniciar sesion.
--- fichaMedica/admin.py
+++ fichaMedica/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Doctor, Secretaria, Paciente
 
-# Register your models here.
+# Registrar Doctor en el Admin
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "nombre", "apellido", "email")
+    search_fields = ("user__rut", "user__first_name", "user__last_name", "user__email")
+    
+    def nombre(self, obj): return obj.user.first_name
+    def apellido(self, obj): return obj.user.last_name
+    def email(self, obj): return obj.user.email
+
+# Registrar Secretaria en el Admin
+@admin.register(Secretaria)
+class SecretariaAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "nombre", "apellido", "email")
+    search_fields = ("user__rut", "user__first_name", "user__last_name", "user__email")
+    
+    def nombre(self, obj): return obj.user.first_name
+    def apellido(self, obj): return obj.user.last_name
+    def email(self, obj): return obj.user.email
+
+# Registrar Paciente en el Admin
+@admin.register(Paciente)
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "nombre", "apellido", "prevision", "telefono")
+    search_fields = ("user__rut", "user__first_name", "user__last_name", "telefono", "user__email")
+    
+    def nombre(self, obj): return obj.user.first_name
+    def apellido(self, obj): return obj.user.last_name
